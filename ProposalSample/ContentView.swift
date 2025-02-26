0981423d0f16af7b4f3671046adf1767d002dedf
taise-hikawa <cmmkarubi@gmail.com>
2025-02-26 11:28:31 +0900

    create proj

---
+++ ProposalSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProposalSample
+//
+//  Created by 樋川大聖 on 2025/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProposalSample/ProposalSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProposalSampleApp.swift
+//  ProposalSample
+//
+//  Created by 樋川大聖 on 2025/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ProposalSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
